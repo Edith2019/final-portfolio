476f6ae6ccd80d82383d26cb3b27961bcab64833
Edith <edith.chevallier@hotmail.fr>
2020-05-12 12:25:07 +0200

    final

--- src/description.js
+++ src/description.js
@@ -14,9 +14,9 @@ export default class Description extends React.Component {
                 <div className="description-container" id="about">
                     <div className="description-text">
                         <h1 className="description-text-t">about</h1>
-                        <p className="description-text-p">Hi! I’m Edith. A fresh fullstack developer just out of Spiced Academy programming bootcamp. Based in Berlin, but available worldwide. i speak javascript, html, css, vue.js, react.js, english, french and sufficient german (ein bier bitte).
+                        <p className="description-text-p">Hi! I’m Edith. A fresh fullstack developer just out of Spiced Academy programming bootcamp. Based in Berlin, but available worldwide. I speak Javascript, HTML, CSS, vue.js, React.js, english, french and sufficient german (ein bier bitte).
 
-                        I’m a quick learner eager to master the tangle of Javascript in a silk css wrap. Currently exploring in-depth frameworks like vue.js and with a huge crush on React.js.
+                        I’m a quick learner eager to master the tangle of Javascript in a silk CSS wrap. Currently exploring in-depth frameworks like vue.js and with a huge crush on React.js.
 
                         My future team will for sure appreciate my curious mindset, great communication skills and my excellent use of gifs. There’s always <a className="description-text-rm" href="https://media1.giphy.com/media/Hc8PMCBjo9BXa/giphy.gif?cid=ecf05e47283247b350d37b41087897244be4ae1f455d4b5f&rid=giphy.gif" rel="noopener noreferrer" target='_blank'> one for the right moment.
                         </a>
