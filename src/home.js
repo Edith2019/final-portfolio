ce53cbefc94a07aa031c1deca85d70a34b56001c
Edith <edith.chevallier@hotmail.fr>
2020-09-23 17:24:18 +0200

    adjusted fadeIn mobile

--- src/home.js
+++ src/home.js
@@ -20,14 +20,14 @@ class Home extends React.Component {
                     </Row>
                     <Row className=" mx-5 bg-warning text-center">
                         <Col className=" my-auto" >
-                            <h1 id="animate" className="fadeInAbout  mt-5 pr-3 display-1 text-white" >
+                            <h1 id="animate" className="fadeInAbout  mt-5 display-1 text-white" >
                                 {t("Welcome")}
                             </h1>
                         </Col>
                     </Row>
                     <Row className="mx-5 pt-2 pb-5 bg-warning text-center">
                         <Col>
-                            <svg id="further" width="6em" height="6em" viewBox="0 0 16 16" className=" fadeInAbout  mr-5 bi bi-chevron-compact-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <svg id="further" width="6em" height="6em" viewBox="0 0 16 16" className=" fadeInAbout  bi bi-chevron-compact-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z" />
                             </svg>
                         </Col>
--- src/navbar.js
+++ src/navbar.js
@@ -5,6 +5,8 @@ import { Link } from "react-scroll";
 import EnFr from "./enFr.js";
 import { withTranslation } from "react-i18next";
 import Container from "react-bootstrap/Container";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
 import Image from "react-bootstrap/Image";
 import PropTypes from "prop-types";
 
@@ -13,6 +15,9 @@ class Header extends React.Component {
         super(props);
         this.handleScroll = this.handleScroll.bind(this);
         this.handleHover = this.handleHover.bind(this);
+        this.openNav = this.openNav.bind(this);
+        this.closeNav = this.closeNav.bind(this);
+        // this.offset = this.offset.bind(this)
         this.state = {
             visible: false
         };
@@ -50,25 +55,45 @@ class Header extends React.Component {
 
     handleLeave() {
         var element = document.getElementById("appear");
-        if (window.pageYOffset >= 20) {
+        if (window.pageYOffset >= 20 && window.screen.width > 450) {
             element.classList.remove("isVisible");
             element.classList.add("notVisible");
         }
     }
+    closeNav() {
+        var collapse = document.getElementsByClassName("navbarCollapse")[0];
+        // document.getElementById("navCur").style.display = "none";
+
+        this.setState({
+
+            visible: !this.state.visible
+
+        })
+
+    }
+
+    openNav() {
+        this.setState({
+
+            visible: !this.state.visible
+
+        })
+    }
+
 
     render() {
         // const visible = this.state.visible;
         const { t } = this.props;
         return (
             <React.Fragment>
-                <Container fluid className="pb-0 pt-4">
+                <Container fluid className="pb-0 pt-4" id="navContainer">
                     <div id="appear" onScroll={() => this.handleScroll()} onMouseEnter={() => this.handleHover()} onMouseLeave={() => this.handleLeave()}>
                         <Navbar collapseOnSelect id="mobileNav" expand="lg" bg="white" color="black" fixed="top" className="navbar" >
                             <Navbar.Brand href="#Home" className="pl-2 pt-0">
                                 <Image src="./assets/BlackTiger.png" width="50" height="50" />
                             </Navbar.Brand>
                             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                            <Navbar.Collapse id="responsive-navbar-nav">
+                            <Navbar.Collapse id="responsive-navbar-nav" className="navbarCollapse">
                                 <Nav className="mr-auto pt-1 " id="navCur">
                                     <Link to="About"
                                         activeClass="active"
@@ -76,7 +101,13 @@ class Header extends React.Component {
                                         smooth={true}
                                         offset={-80}
                                         duration={500}
-                                        className="pr-3 pt-3">{t("About")}
+                                        className="pr-3 pt-3"
+                                        // onClick={this.closeNav}
+                                        data-toggle="collapse"
+                                        data-target=".navbarCollapse"
+
+                                    >
+                                        {t("About")}
                                     </Link>
                                     <Link to="Projects"
                                         activeClass="active"
@@ -84,21 +115,31 @@ class Header extends React.Component {
                                         smooth={true}
                                         offset={-80}
                                         duration={800}
-                                        className="pr-3 pt-3">{t("Projects")}
+                                        className="pr-3 pt-3"
+                                        data-toggle="collapse"
+                                        data-target=".navbarCollapse"
+                                    >{t("Projects")}
                                     </Link>
                                     <Link to="Volunteering"
                                         activeClass="active"
                                         spy={true}
                                         smooth={true}
                                         offset={-80}
-                                        duration={1100} className="pr-3 pt-3">{t("Volunteering")}
+                                        duration={1100} className="pr-3 pt-3"
+                                        data-toggle="collapse"
+                                        data-target=".navbarCollapse"
+                                        data-toggle="collapse"
+                                        data-target=".navbarCollapse">{t("Volunteering")}
                                     </Link>
                                     <Link to="ContactN"
                                         activeClass="active"
                                         spy={true}
                                         smooth={true}
                                         offset={-80}
-                                        duration={1400} className="pr-3 pt-3">{t("Contact")}
+                                        duration={1400} className="pr-3 pt-3"
+                                        data-toggle="collapse"
+                                        data-target=".navbarCollapse"
+                                    >{t("Contact")}
                                     </Link>
                                 </Nav>
                                 <EnFr />
